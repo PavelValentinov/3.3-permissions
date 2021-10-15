97eb139749a62f75a37bec230c0d28c30194ff72
PavelValentinov <81234999+PavelValentinov@users.noreply.github.com>
2021-10-15 15:24:38 +0300

    Update settings.py

--- api_with_restrictions/settings.py
+++ api_with_restrictions/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'permissions',
         'USER': 'postgres',
-        'PASSWORD': '456124',
+        'PASSWORD': '-',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
